cf069952add7991e21ba6f4e1b544327c299c8af
mtrupiano <markt6749@gmail.com>
2021-01-23 15:03:13 -0800

    drafted schema and seed SQL scripts

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id          INT         AUTO_INCREMENT,
+    burger_name VARCHAR(45) NOT NULL,
+    devoured    BOOLEAN     DEFAULT FALSE,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers VALUES
+    (DEFAULT, "Cheeseburger", DEFAULT),
+    (DEFAULT, "Bacon cheeseburger", DEFAULT),
+    (DEFAULT, "Double cheesburger", DEFAULT);
