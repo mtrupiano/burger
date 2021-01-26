0163820cd489985555cf10f3c4c12aec69b82f0c
mtrupiano <markt6749@gmail.com>
2021-01-25 18:30:07 -0800

    heroku setup

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: '192.168.1.24',
-    port: 3306,
-    user: 'newuser',
-    password: 'G1g@sequelpass',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: '192.168.1.24',
+        port: 3306,
+        user: 'newuser',
+        password: 'G1g@sequelpass',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect((err) => {
     if (err) throw err;
